78b342ca612d86e3328f04a99b1c34a343709c84
Pavel Sohanevich <p.sohanevich@nv.dunice.net>
2024-06-13 17:38:23 +0300

    new function

--- index.js
+++ index.js
@@ -1,123 +1,149 @@
-const dom = {
-  new: document.getElementById("new"),
-  add: document.getElementById("add"),
-  tasks: document.getElementById("tasks"),
-  count: document.getElementById("count"),
-};
+
+const newTask = document.getElementById("new");
+const add = document.getElementById("add");
+const tasksText = document.getElementById("tasks");
+const count = document.getElementById("count");
+const allCheckBox = document.querySelector('.main-checkbox');
+const otherCheckBoxes = document.querySelectorAll('.other-checkbox')
+const delAllTasks = document.querySelector('.todo__delete-complete')
 
 const tasks = [];
 
 function createTask() {
-  const newTaskText = dom.new.value;
-  if (newTaskText && isNotHaveTask(newTaskText, tasks)) {
-    addTask(newTaskText, tasks);
-    dom.new.value = "";
-    tasksRender(tasks);
-  }
+    const newTaskText = newTask.value.trim();
+    if (newTaskText && isNotHaveTask(newTaskText, tasks)) {
+        addTask(newTaskText, tasks);
+        newTask.value = "";
+        tasksRender(tasks);
+    }
 }
 
 function renderTasksCount(list) {
-  dom.count.innerHTML = list.length;
+    count.innerHTML = list.length;
 }
-
-dom.add.addEventListener("click", createTask);
 
 function createTaskOnButton(event) {
-  const newTaskText = dom.new.value;
-  if (
-    event.key === "Enter" &&
-    isNotHaveTask(newTaskText, tasks) &&
-    newTaskText
-  ) {
-    addTask(newTaskText, tasks);
-    dom.new.value = "";
-    tasksRender(tasks);
-  }
+    const newTaskText = newTask.value.trim();
+    if (
+        event.key === "Enter" &&
+        isNotHaveTask(newTaskText, tasks) &&
+        newTaskText
+    ) {
+        addTask(newTaskText, tasks);
+        newTask.value = "";
+        tasksRender(tasks);
+    }
 }
 
-document.addEventListener("keydown", createTaskOnButton);
-
 function addTask(text, list) {
-  const task = {
-    id: Date.now(),
-    text,
-    isComplete: false,
-  };
-  list.push(task);
-  console.log(tasks);
+    const task = {
+        id: Date.now(),
+        text,
+        isComplete: false,
+    };
+    list.push(task);
+    console.log(tasks);
 }
 
 function isNotHaveTask(text, list) {
-  let isNotHave = true;
-  list.forEach((task) => {
-    if (task.text === text) {
-      alert("Задача уже существует");
-      isNotHave = false;
-    }
-  });
-  return isNotHave;
+    let isNotHave = true;
+    list.forEach((task) => {
+        if (task.text === text) {
+            alert("Задача уже существует");
+            isNotHave = false;
+        }
+    });
+    return isNotHave;
 }
 
 function tasksRender(list) {
-  let listHTML = "";
+    let listHTML = "";
 
-  list.forEach((task) => {
-    const cls = task.isComplete
-      ? "todo__task todo__task_complete"
-      : "todo__task";
-    const checked = task.isComplete ? "checked" : "";
-    const taskHTML = `
+    list.forEach((task) => {
+        const cls = task.isComplete
+            ? "todo__task todo__task_complete"
+            : "todo__task";
+        const checked = task.isComplete ? "checked" : "";
+        const taskHTML = `
     <div id='${task.id}' class="${cls}">
                     <label class="todo__checkbox"> 
-                     <input type="checkbox" ${checked}>
+                     <input type="checkbox" ${checked} class="other-checkbox">
                      <div class="todo__checkbox-div"></div>
                  </label>
                  <div class="todo__task-text">${task.text}</div>
                  <div class="todo__task-del">-</div>
                  </div>`;
 
-    listHTML += taskHTML;
-  });
-  dom.tasks.innerHTML = listHTML;
+        listHTML += taskHTML;
+    });
+    tasksText.innerHTML = listHTML;
 
-  renderTasksCount(list);
+
+    renderTasksCount(list);
 }
 
 function changeTaskStatus(id, list) {
-  list.forEach((task) => {
-    if (task.id === Number(id)) {
-      task.isComplete = !task.isComplete;
-    }
-  });
+    list.forEach((task) => {
+        if (task.id === Number(id)) {
+            task.isComplete = !task.isComplete;
+        }
+    });
 }
+
+function checkAllTodo(list) {
+    list.forEach((task) => {
+        task.isComplete = !task.isComplete
+    })
+};
 
 function toggleStatus(event) {
-  const target = event.target;
-  const isCheckboxEl = target.classList.contains("todo__checkbox-div");
-  const isDeleteEl = target.classList.contains("todo__task-del");
-  if (isCheckboxEl) {
-    const task = target.parentElement.parentElement;
-    const taskId = task.getAttribute("id");
-    changeTaskStatus(taskId, tasks);
-    tasksRender(tasks);
-  }
-  if (isDeleteEl) {
-    const task = target.parentElement;
-    const taskId = task.getAttribute("id");
-    deleteTask(taskId, tasks);
-    tasksRender(tasks);
-  }
+    const target = event.target;
+    const isCheckboxEl = target.classList.contains("todo__checkbox-div");
+    const isDeleteEl = target.classList.contains("todo__task-del");
+    const isMainCheckbox = target.classList.contains('main-checkbox');
+    if (isCheckboxEl) {
+        const task = target.parentElement.parentElement;
+        const taskId = task.getAttribute("id");
+        changeTaskStatus(taskId, tasks);
+        tasksRender(tasks);
+    }
+    if (isDeleteEl) {
+        const task = target.parentElement;
+        const taskId = task.getAttribute("id");
+        deleteTask(taskId, tasks);
+        tasksRender(tasks);
+    }
+    if (isMainCheckbox) {
+        // const task = target.parentElement.parentElement;
+        // const taskId = task.getAttribute("id");
+        checkAllTodo(tasks);
+        tasksRender(tasks);
+    }
+
 }
 
-dom.tasks.addEventListener("click", toggleStatus);
 
 function deleteTask(id, list) {
-  list.forEach((task, idx) => {
-    if (task.id == id) {
-      list.splice(idx, 1);
-    }
-  });
+    list.forEach((task, idx) => {
+        if (task.id == id) {
+            list.splice(idx, 1);
+        }
+    });
 }
 
-// const del = document.querySelector("todo__task-del");
-// del.parentElement.addEventListener("click", deleteTask);
+// function deleteCompletedTasks(list) {
+//     list.forEach((task, ind) => {
+//         if (task.isComplete = true) {
+//             list.splice(task.filter(item) => );
+//         }
+//     })
+// }
+// delAllTasks.addEventListener('click', deleteCompletedTasks(this.isComplete, tasks))
+add.addEventListener("click", createTask);
+tasksText.addEventListener("click", toggleStatus);
+document.addEventListener("keydown", createTaskOnButton);
+
+
+allCheckBox.addEventListener('click', toggleStatus)
+
+
